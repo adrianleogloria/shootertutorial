9dc2691563a685cba8ef58dcaa2c0c7a596c77be
Tim Eicher <tim@timeicher.com>
2013-06-21 11:57:06 -0500

    Made it thru part 2 of the monogame tutorial: Part 3 is here: http://blogs.msdn.com/b/tarawalker/archive/2013/01/04/windows-8-game-development-using-c-xna-and-monogame-3-0-building-a-shooter-game-walkthrough-part-3-updating-graphics-using-content-pipeline-with-monogame.aspx

---
+++ ShooterTutorial/Game1.cs
@@ -0,0 +1,97 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ShooterTutorial
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager _graphics;
+        SpriteBatch _spriteBatch;
+        private Player _player;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            _player = new Player();
+            
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            // Load the player resources
+
+            Rectangle titleSafeArea = GraphicsDevice.Viewport.TitleSafeArea;
+            var playerPosition = new Vector2(titleSafeArea.X, titleSafeArea.Y + titleSafeArea.Height/2);
+            Texture2D playerTexture = Content.Load<Texture2D>("Graphics\\player");
+            _player.Initialize(playerTexture, playerPosition);
+
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            // Start drawing
+            _spriteBatch.Begin();
+
+            // Draw the Player
+            _player.Draw(_spriteBatch);
+
+            // Stop drawing
+            _spriteBatch.End(); 
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ ShooterTutorial/Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ShooterTutorial
+{
+    class Player
+    {
+        public Texture2D PlayerTexture;
+
+        public Vector2 PlayerPosition;
+
+        // State of the player
+        public bool Active;
+
+        // Amount of hit points the player has
+        public int Health;
+
+        public int Width
+        { get { return PlayerTexture.Width; } }
+        
+        public int Height
+        { get { return PlayerTexture.Height; } }
+
+        public void Initialize(Texture2D playerTexture, Vector2 playerPosition)
+        {
+            PlayerTexture = playerTexture;
+            PlayerPosition = playerPosition;
+
+            Active = true;
+            Health = 100;
+        }
+
+        public void Update()
+        {
+            
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(PlayerTexture, PlayerPosition, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None,
+                             0f);
+        }
+    }
+}
